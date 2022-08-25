0641e0fe98f39707b085d7db6896f69fbe5e805c
Patricia <patyamiga16@hotmail.com>
2022-08-25 15:31:46 -0300

    calculate

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
 function calculateTip(event) {
- alert('Fui submetido para a função.');
+ event.preventDefault();
+ console.log(event);
+ let bill =  document.getElementById('bill').value;
+ let serviceQual = document.getElementById('serviceQual').value;
+ let numOffPeople = document.getElementById('people').value;
+
+    if(bill == '' | serviceQual == 0) {
+        alert("Por favor, preencha os valores")
+        return;
+    }
+
+    if(numOffPeople == "" | numOffPeople <=1) {
+        numOffPeople =1;
+        document.getElementById('each').style.display = "none";
+    }else{
+        document.getElementById('each').style.display = "block";
+    }
+
+    let total = (bill * serviceQual) / numOffPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
 }
 
 document.getElementById('totalTip').style.display = "none";
